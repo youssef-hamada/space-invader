1e129f143b29663c228a24e44c6fcfa8f223b140
unknown <hamaday468@gmail.com>
2025-04-06 17:42:49 +0200

    finish aliens add game over add score

--- space.js
+++ space.js
@@ -37,6 +37,16 @@ let alienCount = 0;
 
 let alienVelocityX = 1;
 
+//bullets
+let bulletArray = [];
+let bulletVelocityY = -10;
+
+//game
+let gameOver = false;
+
+//score
+let score = 0;
+
 window.onload = () => {
   board = document.getElementById("board");
   board.width = boardWidth;
@@ -56,31 +66,87 @@ window.onload = () => {
 
   requestAnimationFrame(update);
   document.addEventListener("keydown", moveShip);
+  document.addEventListener("keyup", shoot);
 };
 
 function update() {
   requestAnimationFrame(update);
 
+  if (gameOver) {
+    return;
+  }
+
   context.clearRect(0, 0, boardWidth, boardHeight);
 
   context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
 
+  //render aliens
   for (let i = 0; i < alienArray.length; i++) {
     let alien = alienArray[i];
     if (alien.alive) {
       alien.x += alienVelocityX;
+
       if (alien.x + alien.width >= boardWidth || alien.x <= 0) {
         alienVelocityX *= -1;
+        alien.x += alienVelocityX * 2;
         for (let j = 0; j < alienArray.length; j++) {
           alienArray[j].y += alienHeight;
         }
       }
       context.drawImage(alien.Img, alien.x, alien.y, alien.width, alien.height);
+      if (alien.y >= ship.y) {
+        gameOver = true;
+        context.fillStyle = "red";
+        context.font = "30px Arial";
+        context.fillText("Game Over", boardWidth / 2 - 70, boardHeight / 2);
+      }
     }
   }
+
+  // render bullets
+  for (let i = 0; i < bulletArray.length; i++) {
+    let bullet = bulletArray[i];
+    bullet.y += bulletVelocityY;
+    context.fillStyle = "white";
+    context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+
+    for (let j = 0; j < alienArray.length; j++) {
+      let alien = alienArray[j];
+      if (!bullet.used && alien.alive && detectCollision(bullet, alien)) {
+        bullet.used = true;
+        alien.alive = false;
+        alienCount--;
+        score += 10;
+      }
+    }
+  }
+
+  while (
+    bulletArray.length > 0 &&
+    (bulletArray[0].used || bulletArray[0].y < 0)
+  ) {
+    bulletArray.shift();
+  }
+
+  if (alienCount == 0) {
+    alienCols = Math.min(alienCols + 1, columns / 2 - 2);
+    alienRows = Math.min(alienRows + 1, rows - 2);
+    alienVelocityX += 0.3;
+    alienArray = [];
+    bulletArray = [];
+    createAliens();
+  }
+
+  context.fillStyle = "white";
+  context.font = "20px Arial";
+  context.fillText("Aliens left: " + alienCount, 5, 40);
+  context.fillText(score, 5, 20);
 }
 
 function moveShip(e) {
+  if (gameOver) {
+    return;
+  }
   if (e.code == "ArrowLeft" && ship.x - shipVelocityX >= 0) {
     ship.x -= shipVelocityX;
   } else if (
@@ -106,4 +172,29 @@ function createAliens() {
     }
   }
   alienCount = alienArray.length;
+}
+
+function shoot(e) {
+  if (gameOver) {
+    return;
+  }
+  if (e.code == "Space") {
+    let bullet = {
+      x: ship.x + (shipWidth * 15) / 32,
+      y: ship.y,
+      width: ship.width / 8,
+      height: ship.height / 2,
+      used: false,
+    };
+    bulletArray.push(bullet);
+  }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
 }
